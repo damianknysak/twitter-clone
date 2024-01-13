17d72d6e27eda5e75ba233b1e85c45b9c0ff1163
damianknysak <damian11rs@onet.pl>
2024-01-14 00:02:34 +0100

    header layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainSectionComponent } from './components/main-section/main-section.component';
 import { SideHeaderComponent } from './components/side-header/side-header.component';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainSectionComponent,
     SideHeaderComponent,
-    SidePanelComponent
+    SidePanelComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/side-header/side-header.component.ts
+++ src/app/components/side-header/side-header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+  faEllipsis,
+  faHouse,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-header',
   templateUrl: './side-header.component.html',
-  styleUrls: ['./side-header.component.css']
+  styleUrls: ['./side-header.component.css'],
 })
 export class SideHeaderComponent {
-
+  twitterIcon = faXTwitter;
+  homeIcon = faHouse;
+  searchIcon = faSearch;
+  userIcon = faUser;
+  ellipsisIcon = faEllipsis;
 }
