02d25691b33d38f5dedcf1a31bcd98264299f68d
damianknysak <damian11rs@onet.pl>
2024-01-14 01:13:19 +0100

    side panel styling

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
-  styleUrls: ['./side-panel.component.css']
+  styleUrls: ['./side-panel.component.css'],
 })
 export class SidePanelComponent {
-
+  searchIcon = faSearch;
 }
