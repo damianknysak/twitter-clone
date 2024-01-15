923d8617d2464be1ca7fac3257f2a113020f2a1e
damianknysak <damian11rs@onet.pl>
2024-01-15 15:36:29 +0100

    post layout

--- src/app/components/shared/post/post.component.ts
+++ src/app/components/shared/post/post.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
+import {
+  faComment,
+  faHeart,
+  faShareSquare,
+} from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -8,4 +14,7 @@ import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 })
 export class PostComponent {
   ellipsisIcon = faEllipsis;
+  heartIcon = faHeart;
+  commentIcon = faComment;
+  shareIcon = faShareSquare;
 }
