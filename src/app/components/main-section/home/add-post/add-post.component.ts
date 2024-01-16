2ce29314e098f69cb09d4f799654d136640c792c
damianknysak <damian11rs@onet.pl>
2024-01-16 15:51:04 +0100

    home file structure

--- src/app/components/main-section/home/add-post/add-post.component.ts
+++ src/app/components/main-section/home/add-post/add-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faImage } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
 })
 export class AddPostComponent {
-
+  galleryIcon = faImage;
 }
--- src/app/components/main-section/home/home-navigation/home-navigation.component.ts
+++ src/app/components/main-section/home/home-navigation/home-navigation.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-navigation',
   templateUrl: './home-navigation.component.html',
-  styleUrls: ['./home-navigation.component.css']
+  styleUrls: ['./home-navigation.component.css'],
 })
 export class HomeNavigationComponent {
-
+  activeSection = 'for-you';
+  changeSection(section: string) {
+    this.activeSection = section;
+  }
 }
