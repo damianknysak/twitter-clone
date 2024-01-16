8408dd6416ed196e6043e3901f18179ad3ca72d6
damianknysak <damian11rs@onet.pl>
2024-01-16 15:21:50 +0100

    main-section new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExploreComponent } from './components/main-section-components/explore/explore.component';
-import { HomeComponent } from './components/main-section-components/home/home.component';
-import { ProfileComponent } from './components/main-section-components/profile/profile.component';
+import { ExploreComponent } from './components/main-section/explore/explore.component';
+import { HomeComponent } from './components/main-section/home/home.component';
+import { ProfileComponent } from './components/main-section/profile/profile.component';
 
 const routes: Routes = [
-  {path: "",
-  component: HomeComponent},
-  {path: "explore",
-  component: ExploreComponent},
-  {path: "profile",
-  component: ProfileComponent },
+  { path: '', component: HomeComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { MainSectionComponent } from './components/main-section/main-section.com
 import { SideHeaderComponent } from './components/side-header/side-header.component';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './components/main-section-components/home/home.component';
-import { ExploreComponent } from './components/main-section-components/explore/explore.component';
-import { ProfileComponent } from './components/main-section-components/profile/profile.component';
+import { HomeComponent } from './components/main-section/home/home.component';
+import { ExploreComponent } from './components/main-section/explore/explore.component';
+import { ProfileComponent } from './components/main-section/profile/profile.component';
 import { PostComponent } from './components/shared/post/post.component';
 
 @NgModule({
