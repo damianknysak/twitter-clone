46e78dae165db2ea883be925d47c901d49eac942
damianknysak <damian11rs@onet.pl>
2024-01-17 22:36:35 +0100

    route change

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.css'],
 })
-export class SidePanelComponent {}
+export class SidePanelComponent {
+  showSearchPanel = true;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((val: any) => {
+        if (val.url === '/explore') {
+          this.showSearchPanel = false;
+        } else {
+          this.showSearchPanel = true;
+        }
+      });
+  }
+}
